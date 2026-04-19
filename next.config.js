10344e876ce6b6e7f97532114282a9c90233344e
koukal96 <r1mail@seznam.cz>
2026-04-19 20:44:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Kukido', // <-- SEM NAPIŠ PŘESNÝ NÁZEV TVÉHO REPOZITÁŘE
   images: {
     unoptimized: true,
   }
